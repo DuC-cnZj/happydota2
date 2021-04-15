d1307ee0a2b71852205d32abb3623a3895bfc888
duc <1025434218@qq.com>
2021-04-15 16:13:12 +0800

    wip

--- src/components/NotFound.tsx
+++ src/components/NotFound.tsx
@@ -1,3 +1,12 @@
-const NotFound: React.FC = () => (<>404 not found</>);
+import img from "../dota2/404-two.png";
+
+const NotFound: React.FC = () => (
+  <div className="not-found">
+    <img src={img} alt="404" />
+    <div className="not-found-title">
+      <h1>404</h1>
+    </div>
+  </div>
+);
 
 export default NotFound;
