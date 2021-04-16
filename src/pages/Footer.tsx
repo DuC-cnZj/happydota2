82d1aab172684576887dc62a4aea07b71f9da361
duc <1025434218@qq.com>
2021-04-17 00:01:41 +0800

    wip

--- src/pages/Footer.tsx
+++ src/pages/Footer.tsx
@@ -11,35 +11,35 @@ const Footer: React.FC = () => {
       <div className="footer-md">dota2 刀圈欢乐多 by duc @2021.</div>
       <div className="footer-sm container">
         <div className="footer-item">
-          <Tooltip placement="bottom" title="home" className="tooltip">
+          <Tooltip placement="bottom" title="主页" className="tooltip">
             <Link to="/" className="footer-menu-item">
               <HomeOutlined />
             </Link>
           </Tooltip>
         </div>
         <div className="footer-item">
-          <Tooltip placement="bottom" title="物品" className="tooltip">
+          <Tooltip placement="top" title="物品" className="tooltip">
             <Link to="/equipment" className="footer-menu-item">
               <img src={sj} alt="sj" style={{ width: "80%" }} />
             </Link>
           </Tooltip>
         </div>
         <div className="footer-item">
-          <Tooltip placement="bottom" title="英雄" className="tooltip">
+          <Tooltip placement="top" title="英雄" className="tooltip">
             <Link to="/heros" className="footer-menu-item">
               <img src={gg} alt="gg" style={{ width: "80%" }} />
             </Link>
           </Tooltip>
         </div>
         <div className="footer-item">
-          <Tooltip placement="bottom" title="搞笑锦集" className="tooltip">
+          <Tooltip placement="top" title="搞笑锦集" className="tooltip">
             <Link to="/funny" className="footer-menu-item">
               <img src={puck} alt="puck" style={{ width: "80%" }} />
             </Link>
           </Tooltip>
         </div>
         <div className="footer-item">
-          <Tooltip placement="bottom" title="user" className="tooltip">
+          <Tooltip placement="top" title="用户" className="tooltip">
             <Link to="/user" className="footer-menu-item">
               <UserOutlined />
             </Link>
