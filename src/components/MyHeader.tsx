17b50035ec286496a375f8d767cde44e8d025d5d
duc <1025434218@qq.com>
2021-04-20 08:10:09 +0800

    wip

--- src/components/MyHeader.tsx
+++ src/components/MyHeader.tsx
@@ -101,9 +101,12 @@ const Header: React.FC<IProps> = ({
         </div>
         <div className="header-right">
           {user.isLogin ? (
-            <div>
-              <AvatarSm url={user.avatarUrl} user={user} />
-              <AvatarMd url={user.avatarUrl} user={user} />
+            <div >
+              <AvatarSm url={user.avatarUrl} user={user}/>
+              <div className="md-avatar-login-group">
+                <NotificationMd/>
+                <AvatarMd url={user.avatarUrl} user={user} />
+              </div>
             </div>
           ) : (
             <div onClick={showModal}>
@@ -199,28 +202,52 @@ const Header: React.FC<IProps> = ({
             </div>
           </Modal>
         </div>
-        <div className="header-notification">
-          <Tooltip title="消息通知" placement="bottom">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
-              />
-            </svg>
-          </Tooltip>
-        </div>
+        <Notification/>
       </div>
     </>
   );
 };
+
+const Notification = () => (
+  <div className="header-notification">
+    <Tooltip title="消息通知" placement="bottom">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="h-6 w-6"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth={2}
+          d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
+        />
+      </svg>
+    </Tooltip>
+  </div>
+);
+const NotificationMd = () => (
+  <div className="header-notification-md">
+    <Tooltip title="消息通知" placement="bottom">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="h-6 w-6"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth={2}
+          d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
+        />
+      </svg>
+    </Tooltip>
+  </div>
+);
 
 interface State {
   loginModal: {
