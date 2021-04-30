240372b8f09e3c86f2dbe03e4326d7b8a8cb96b2
duc <1025434218@qq.com>
2021-04-30 08:27:06 +0800

    wip

---
+++ src/components/DetailPreview.tsx
@@ -0,0 +1,60 @@
+import React, { useRef, useState } from "react";
+import { Carousel, Image, Card } from "antd";
+import st from "../dota2/st.jpeg";
+import xg from "../dota2/xg.jpeg";
+import nw from "../dota2/nw.jpeg";
+import sf from "../dota2/sf.jpeg";
+import ta from "../dota2/ta.jpeg";
+import classNames from "classnames";
+import { CarouselRef } from "antd/lib/carousel";
+
+interface Image {
+  url: string;
+}
+
+const DetailPreviewCard: React.FC = () => {
+  const images: string[] = [xg, st, nw, sf, ta, xg, st, nw, sf, ta];
+  const s = useRef<CarouselRef>(null);
+  const [current, setCurrent] = useState<number>(0);
+
+  return (
+    <Card className="detail-preview-card">
+      <Image.PreviewGroup>
+        <Carousel
+          ref={s}
+          effect="fade"
+          // autoplay={true}
+          beforeChange={(from, to) => {
+            setCurrent(to);
+            console.log(to);
+          }}
+          style={{
+            overflow: "hidden",
+            width: "100%",
+            height: "400rem",
+            marginBottom: "10rem",
+          }}
+        >
+          {images.map((item, index) => (
+            <div className="detail-preview-card__item" key={index}>
+              <Image src={item} />
+            </div>
+          ))}
+        </Carousel>
+      </Image.PreviewGroup>
+      <div style={{ display: "flex" }} className="detail-preview-card__bottom">
+        {images.map((item, index) => (
+          <div
+            className={classNames("detail-preview-card__bottom__image", {
+              "detail-preview-card__bottom__image--selected": index === current,
+            })}
+            style={{ background: "url(" + item + ")" }}
+            onClick={() => s.current?.goTo(index)}
+          ></div>
+        ))}
+      </div>
+    </Card>
+  );
+};
+
+export default DetailPreviewCard;
--- src/pages/Detail.tsx
+++ src/pages/Detail.tsx
@@ -1,14 +1,22 @@
-import { Card, Col, Row, Button, Tabs } from "antd";
+import { Image, Card, Col, Row, Button, Tabs } from "antd";
+import classNames from "classnames";
+import { useState } from "react";
+import DetailPreviewCard from "../components/DetailPreview";
 
 const { TabPane } = Tabs;
 
 const Detail: React.FC = () => {
+  const [more, setMore] = useState<boolean>(false);
+
   return (
     <div className="detail">
       <div className="container">
         <Row>
           <Col md={24}>
-            <Card bordered={false} style={{ width: "100%" }}>
+            <Card
+              bordered={false}
+              style={{ width: "100%", marginBottom: "20rem" }}
+            >
               <div className="detail-header">
                 <div className="detail-header-top">
                   <div className="detail-header__left">
@@ -31,7 +39,7 @@ const Detail: React.FC = () => {
                           评价
                         </Button>
                         <Button type="primary" className="sourcestation">
-                          屈原站
+                          去打一把
                         </Button>
                       </div>
                     </div>
@@ -58,10 +66,74 @@ const Detail: React.FC = () => {
             </Card>
           </Col>
         </Row>
-        <Row>
+        <Row gutter={[16, 16]}>
           <Col md={18}>
-            <Row>top</Row>
-            <Row>di</Row>
+            <Row style={{ marginBottom: "15rem" }}>
+              <Col span={24}>
+                <DetailPreviewCard />
+              </Col>
+            </Row>
+            <Row style={{ marginBottom: "15rem" }}>
+              <Col span={24}>
+                <Card style={{ paddingBottom: "20rem" }}>
+                  <div
+                    className={classNames("detail-content", {
+                      "detail-content--less": !more,
+                    })}
+                  >
+                    <p>
+                      ◇クトゥルフ神話TRPG◇ 「監獄館殺人事件」
+                      本格閉鎖館モノミステリー
+                    </p>
+                    <p>あなたもミステリー小説の登場人物に</p>
+                    <p>この殺人事件には超常的な抜け道は一切存在しない ▼物語</p>
+                    <p>探偵事務所に一人の男が訪ねてくる。</p>
+                    <p>「どうか、魔犬の正体を暴いてくれませんか」</p>
+                    <p>謎に包まれた連続殺人事件。</p>
+                    <p>すべてはある一家の周りで巻き起こっていた。</p>
+                    <p>
+                      あなた方は数奇な運命を辿る様に奇妙な館へと足を踏み入れる。
+                    </p>
+                    <p>
+                      ▼HO（ハンドアウト） 【探偵】
+                      あなたは探偵事務所を構えた探偵だ。
+                    </p>
+                    <p>
+                      また、過去に何らかの事件を解決した経験がある。 【助手】
+                    </p>
+                    <p>
+                      あなたはHO探偵の助手だ。 共に調査を行っている。 【刑事】
+                    </p>
+                    <p>
+                      あなたは捜査第一課に所属する刑事だ。
+                      とある事件を追っている。
+                    </p>
+                    <p>▼概要 KP難易度：★★★★☆ PL難易度：★★★★☆</p>
+                    <p>舞台設定：現代日本クローズド「監獄館」 推奨人数：3人</p>
+                    <p>
+                      推定時間：8時間から12時間
+                      共通推奨：〈目星〉〈聞き耳〉〈図書館〉
+                    </p>
+                    <p>共通準推奨：〈心理学〉〈生物学〉〈機械修理〉〈説得〉</p>
+                    <p>ロスト・発狂：有り（かなり高い）</p>
+                  </div>
+                  <Button
+                    type={!more ? "primary" : "ghost"}
+                    style={{ marginTop: "8rem" }}
+                    onClick={() => setMore(!more)}
+                  >
+                    {more ? "收起来" : "显示全部"}
+                  </Button>
+                </Card>
+              </Col>
+            </Row>
+            <Row style={{ marginBottom: "15rem" }}>
+              <Col span={24}>
+                <Card style={{paddingBottom: "20rem"}}>
+                  评价区域
+                </Card>
+              </Col>
+            </Row>
           </Col>
           <Col md={6}>right</Col>
         </Row>
